bc1255adec52017f7625f66eaacd897dee3131a6
Sahan Maduwantha <madumax2006@gmail.com>
2026-06-12 12:16:48 +0530

    Add files via upload

---
+++ assets/js/config/google-auth-config.js
@@ -0,0 +1,3 @@
+// Paste your Google OAuth 2.0 Web Client ID between the quotes below.
+// Google Cloud Console > APIs & Services > Credentials > OAuth 2.0 Client IDs
+window.ENP_GOOGLE_CLIENT_ID = "1099027502678-lau6lrsec40lnil7j425c62vt77r60gb.apps.googleusercontent.com";
